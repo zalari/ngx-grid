cd4937fc1b2cce0414b4de15db7096622ea0da22
David Enke <david.enke@external.t-systems.com>
2018-06-11 11:19:31 +0200

    fixed build scripts

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -36,11 +36,11 @@ cd ${DIR_SRC}/../
 cp -rf dist/* ${DIR_EXEC}/${DEMO_DIR}/node_modules/@zalari/ngx-grid
 
 # buid the demo app
-npm run build --prod
+cd ${DIR_EXEC}/${DEMO_DIR} && npm run build --prod
 
 # copy the built folder to target
 rm -rf ${DIR_EXEC}/${DEMO_DIST}
-mv ${DEMO_DIR}/dist ${DIR_EXEC}/${DEMO_DIST}
+mv ${DIR_EXEC}/${DEMO_DIR}/dist/* ${DIR_EXEC}/${DEMO_DIST}
 
 # remove working directory
 # rm -rf ${DIR_EXEC}/${DEMO_DIR}
