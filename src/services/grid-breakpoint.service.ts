07978dd9a97e989718beff8362397254a0ca8df2
David Enke <david.enke@zalari.de>
2018-07-13 18:45:05 +0200

    ensured that breakpoints are sorted, removed enum to allow generic usage

--- src/services/grid-breakpoint.service.ts
+++ src/services/grid-breakpoint.service.ts
@@ -4,7 +4,6 @@ import { Inject, Injectable } from '@angular/core';
 
 import { GRID_BREAKPOINTS } from '../injection-tokens/grid-breakpoints.injection-token';
 import { GridBreakpoints } from '../interfaces/grid-breakpoints.interface';
-import { Breakpoint } from '../enums/breakpoint.enum';
 
 export const INJECTED_STYLE_ID = 'ngx-grid-style';
 
@@ -13,26 +12,37 @@ export const INJECTED_STYLE_ID = 'ngx-grid-style';
 })
 export class GridBreakpointService {
 
-  private _queryListeners = new Map<Breakpoint, MediaQueryList>();
+  private readonly _smallestBreakpoint: string;
 
-  private _currentBreakpoint = new BehaviorSubject<Breakpoint>(Breakpoint.ExtraSmall);
+  private readonly _breakpoints = new Map<string, number>();
+
+  private _currentBreakpoint: BehaviorSubject<string>;
+
+  private _queryListeners = new Map<string, MediaQueryList>();
 
   private get _isStyleInjected(): boolean {
     return window.document.getElementById(INJECTED_STYLE_ID) !== null;
   }
 
-  get currentBreakpoint(): Observable<Breakpoint> {
+  get currentBreakpoint(): Observable<string> {
     return this._currentBreakpoint.asObservable();
   }
 
-  constructor(@Inject(GRID_BREAKPOINTS) private _gridBreakpoints: GridBreakpoints) {
+  constructor(@Inject(GRID_BREAKPOINTS) private readonly _gridBreakpoints: GridBreakpoints) {
+    // ensure breakpoint sorting
+    this._breakpoints = this._sortBreakpoints(this._gridBreakpoints);
+
+    // initialize current breakpoint from smallest entry
+    this._smallestBreakpoint = Array.from(this._breakpoints.keys())[0];
+    this._currentBreakpoint = new BehaviorSubject(this._smallestBreakpoint);
+
     // initialize the listener
     this._initializeQueryListeners();
   }
 
   // initializes the grid by injecting styles to the head section
   initialize(): Promise<void> {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       if (!this._isStyleInjected) {
         const head: HTMLHeadElement = window.document.querySelector('head');
         const style: HTMLStyleElement = window.document.createElement('style');
@@ -52,7 +62,7 @@ export class GridBreakpointService {
             --grid-col-start: auto;
             --grid-row-start: auto;
           }
-        
+
           .ngx-grid {
             display: grid;
             grid-auto-flow: row dense;
@@ -61,7 +71,7 @@ export class GridBreakpointService {
             grid-template-columns: repeat(var(--grid-cols), 1fr);
             grid-template-rows: repeat(var(--grid-rows), 1fr);
 
-            @media ${this._buildQuery(Breakpoint.ExtraSmall)} {
+            @media ${this._buildQuery(this._smallestBreakpoint)} {
               /* we want _no_ equal heights on the smallest viewport because usually we have only one column here... */
               grid-auto-rows: min-content;
             }
@@ -74,9 +84,8 @@ export class GridBreakpointService {
         `;
 
         // add media queries
-        Object
-          .keys(this._gridBreakpoints)
-          .forEach((breakpoint: Breakpoint) => {
+        this._breakpoints
+          .forEach((value, breakpoint) => {
             styleDefinitions += `
               /* prepare all breakpoints by updating... */
               @media ${this._buildQuery(breakpoint)} {
@@ -87,7 +96,7 @@ export class GridBreakpointService {
                     /* ... and the columns count */
                     --grid-cols: var(--grid-cols-${breakpoint});
                     --grid-rows: var(--grid-rows-${breakpoint});
-                    
+
                     /* ... and the col and row spans and offsets */
                     --grid-col-start: var(--grid-col-start-${breakpoint});
                     --grid-col-span: var(--grid-col-span-${breakpoint});
@@ -112,10 +121,9 @@ export class GridBreakpointService {
   }
 
   // registers a media query listener
-  registerListener(breakpoint: Breakpoint, listener: MediaQueryListListener) {
+  registerListener(breakpoint: string, listener: MediaQueryListListener) {
     // return if breakpoint is unknown
-    if (!Object.values(Breakpoint)
-      .includes(breakpoint)) {
+    if (!this._breakpoints.has(breakpoint)) {
       throw new Error(`The breakpoint "${breakpoint}" is unknown`);
     }
 
@@ -125,15 +133,15 @@ export class GridBreakpointService {
     }
 
     // register the listener for the specific breakpoint
-    this._queryListeners.get(breakpoint)
+    this._queryListeners
+      .get(breakpoint)
       .addListener(listener);
   }
 
   // unregisters a media query listener
-  unregisterListener(breakpoint: Breakpoint, listener: MediaQueryListListener) {
+  unregisterListener(breakpoint: string, listener: MediaQueryListListener) {
     // return if breakpoint is unknown
-    if (!Object.values(Breakpoint)
-      .includes(breakpoint)) {
+    if (!this._breakpoints.has(breakpoint)) {
       throw new Error(`The breakpoint "${breakpoint}" is unknown`);
     }
 
@@ -143,45 +151,54 @@ export class GridBreakpointService {
     }
 
     // register the listener for the specific breakpoint
-    this._queryListeners.get(breakpoint)
+    this._queryListeners
+      .get(breakpoint)
       .removeListener(listener);
   }
 
-  private _initializeQueryListeners() {
+  private _sortBreakpoints(breakpoints: GridBreakpoints): Map<string, number> {
+    // https://stackoverflow.com/a/16794116/1146207
+    const sortedBreakpoints = new Map<string, number>();
     Object
-      .keys(this._gridBreakpoints)
-      .forEach((breakpoint: Breakpoint) => {
+      .keys(breakpoints)
+      .sort((a, b) => breakpoints[a] - breakpoints[b])
+      .forEach(breakpoint => {
+        sortedBreakpoints.set(breakpoint, breakpoints[breakpoint]);
+      });
+
+    return sortedBreakpoints;
+  }
+
+  private _initializeQueryListeners() {
+    this._breakpoints
+      .forEach((value, breakpoint) => {
         // create listener and keep reference
         const query = this._buildQuery(breakpoint);
         const queryListener = window.matchMedia(query);
         this._queryListeners.set(breakpoint, queryListener);
 
         // register internal listener
-        queryListener.addListener((queryList) => this._breakpointQueryListener(breakpoint, queryList));
-
-        // call listener initially
-        this._breakpointQueryListener(breakpoint, queryListener);
+        queryListener.addListener(() => this._findCurrentBreakpoint());
       });
+
+    // find current breakpoint initially
+    this._findCurrentBreakpoint();
   }
 
-  private _breakpointQueryListener(breakpoint: Breakpoint, queryList: MediaQueryList) {
-    // if the query matches, the breakpoint is active
-    if (queryList.matches) {
-      this._currentBreakpoint.next(breakpoint);
-    }
-    // if not, the next smaller one is active
-    else {
-      this._currentBreakpoint.next(this._previousBreakpoint(breakpoint));
-    }
+  private _findCurrentBreakpoint() {
+    // find the currently matching breakpoint
+    const matchingBreakpoint = Array
+       .from(this._queryListeners.keys())
+       // the last breakpoint that matches, fallback to smallest
+       .reduce((previousBreakpoint, currentBreakpoint) => {
+         return this._queryListeners.get(currentBreakpoint).matches ? currentBreakpoint : previousBreakpoint;
+       }, this._smallestBreakpoint);
+
+    // update the subject
+    this._currentBreakpoint.next(matchingBreakpoint);
   }
 
-  private _previousBreakpoint(breakpoint: Breakpoint): Breakpoint | undefined {
-    const breakpoints = Object.values(Breakpoint);
-    const currentIndex = breakpoints.indexOf(breakpoint);
-    return breakpoints[currentIndex - 1];
-  }
-
-  private _buildQuery(breakpoint: Breakpoint): string {
+  private _buildQuery(breakpoint: string): string {
     return `(min-width: ${this._gridBreakpoints[breakpoint]}px)`;
   }
 
