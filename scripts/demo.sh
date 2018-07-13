e4fdb1cdc1027f58e220750e4047f817f92a33c2
David Enke <david.enke@zalari.de>
2018-07-13 18:45:45 +0200

    remove argument

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -16,20 +16,20 @@ COLOR_YELLOW='\033[0;32m'
 ng new ${DEMO_NAME} --directory=${DEMO_DIR} --style=scss --routing=false --force --skip-git --skip-tests
 
 ## replace the assets
-echo -e "${COLOR_YELLOW}COPY${COLOR_NONE} ${DEMO_DIR}/src/app/app.component.html"
+echo "${COLOR_YELLOW}COPY${COLOR_NONE} ${DEMO_DIR}/src/app/app.component.html"
 cp ${DIR_SRC}/demo/app.component.html ${DIR_EXEC}/${DEMO_DIR}/src/app/app.component.html
 
-echo -e "${COLOR_YELLOW}COPY${COLOR_NONE} ${DEMO_DIR}/src/app/app.component.ts"
+echo "${COLOR_YELLOW}COPY${COLOR_NONE} ${DEMO_DIR}/src/app/app.component.ts"
 cp ${DIR_SRC}/demo/app.component.ts ${DIR_EXEC}/${DEMO_DIR}/src/app/app.component.ts
 
-echo -e "${COLOR_YELLOW}COPY${COLOR_NONE} ${DEMO_DIR}/src/app/app.module.ts"
+echo "${COLOR_YELLOW}COPY${COLOR_NONE} ${DEMO_DIR}/src/app/app.module.ts"
 cp ${DIR_SRC}/demo/app.module.ts ${DIR_EXEC}/${DEMO_DIR}/src/app/app.module.ts
 
-echo -e "${COLOR_YELLOW}COPY${COLOR_NONE} ${DEMO_DIR}/src/styles.scss"
+echo "${COLOR_YELLOW}COPY${COLOR_NONE} ${DEMO_DIR}/src/styles.scss"
 cp ${DIR_SRC}/demo/styles.scss ${DIR_EXEC}/${DEMO_DIR}/src/styles.scss
 
 # copy the built library
-echo -e "${COLOR_YELLOW}COPY${COLOR_NONE} ${DEMO_DIR}/node_modules/@zalari/ngx-grid"
+echo "${COLOR_YELLOW}COPY${COLOR_NONE} ${DEMO_DIR}/node_modules/@zalari/ngx-grid"
 rm -rf ${DIR_EXEC}/${DEMO_DIR}/node_modules/@zalari/ngx-grid
 mkdir -p ${DIR_EXEC}/${DEMO_DIR}/node_modules/@zalari/ngx-grid
 cd ${DIR_SRC}/../
